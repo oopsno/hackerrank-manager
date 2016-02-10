77e89c1756cb2e898907e13560598f93920f8179
Oopsno <yufeiliu94@gmail.com>
2016-02-10 17:16:52 +0800

    improved utest

--- src/HRank/Utilities/Manager.hs
+++ src/HRank/Utilities/Manager.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Language.Haskell.Exts.Parser
 import Language.Haskell.Exts.Syntax
 import System.IO
+import System.Exit
 import System.Directory
 import System.FilePath.Posix
 import System.Environment
@@ -36,32 +37,36 @@ copySource path = readFile path >>= copyStringToPasteboard . adjustCode
 
 -- X
 
-executeOnTheFly :: String -> IO ()
-executeOnTheFly src = do
-  cwd <- getCurrentDirectory
-  setCurrentDirectory "src"
-  (p, h) <- mkstemps "Main" ".hs"
-  let cmd = "ghc -o Main " ++ p
-  hPutStrLn h src
-  hClose h
+compile :: FilePath -> IO ()
+compile src = do
+  tmpdir <- mkdtemp "GHCTemp"
+  let cmd = unwords ["ghc", "--make", "-isrc", "-hidir", tmpdir, "-odir", tmpdir, src]
   putStrLn $ "[hrmng][Compile]: " ++ cmd
-  callCommand cmd
-  putStrLn "[hrmng][Excute]: Main"
-  catch (callCommand "./Main")
-        (\e -> do let err = show (e :: IOException)
-                  hPutStrLn stderr ("[hrmng][Errro]: Terminated with non-zero exit code: " ++ err)
-                  return ())
-  putStrLn "[hrmng][Cleanup]: removing temporary files"
-  removeFile p
-  removeFile (replaceExtension p "hi")
-  removeFile (replaceExtension p "o")
-  removeFile "Main"
-  setCurrentDirectory cwd
+  catch (callCommand cmd)
+        (\e -> hPutStrLn stderr $ "[hrmng][Errro]: Compile Failed: " ++ show (e :: IOException))
+
+  removeDirectoryRecursive tmpdir
+
+executeOnTheFly :: String -> (FilePath -> IO()) -> IO ()
+executeOnTheFly src action = do
+  (p, h) <- mkstemps "Main" ".hs"
+  let bin = takeBaseName p
+  hPutStrLn h src
+  hFlush h
+  compile p
+  action bin
+  hClose h
+  putStrLn $ unwords ["[hrmng][Cleanup] removing: ", p, bin]
+  mapM_ removeFile [p, bin]
+
+runhaskell :: String -> IO ()
+runhaskell src = executeOnTheFly src $ \p -> do
+  putStrLn $ "[hrmng][Excute]: " ++ p
+  catch (callProcess ("./" ++ p) [])
+        (\e -> hPutStrLn stderr $ "[hrmng][Errro]: Terminated with non-zero exit code:" ++ show (e :: IOException))
 
 executeMain :: FilePath -> IO ()
-executeMain path = do
-  src <- readFile path
-  executeOnTheFly $ adjustCode src
+executeMain path = readFile path >>= runhaskell . adjustCode
 
 detectModuleName :: FilePath -> IO String
 detectModuleName path = do
@@ -78,16 +83,23 @@ detectModuleName path = do
 
 runUTest :: FilePath -> IO ()
 runUTest path = do
-  module_s <- detectModuleName path
-  module_t <- detectModuleName $ replaceBaseName path "UnitTest"
-  let main_src = [ "module Main where"
-                 , "import qualified " ++ module_s ++ " as S"
-                 , "import qualified " ++ module_t ++ " as T"
-                 , "import qualified HRank.Utilities.UnitTest as U"
-                 , "main :: IO ()"
-                 , "main = U.runTest S.main T.main" ]
-  executeOnTheFly $ unlines main_src
-
+  solutionSrc <- readFile path
+  moduleT <- detectModuleName $ replaceBaseName path "UnitTest"
+  executeOnTheFly (adjustCode solutionSrc) $ \p -> do
+    let mainSrc = [ "module Main where"
+                  , "import qualified " ++ moduleT ++ " as U"
+                  , "import qualified Data.Text as T"
+                  , "import Test.Hspec"
+                  , "import Test.QuickCheck"
+                  , "import System.Process"
+                  , "sample :: IO ()"
+                  , "sample = hspec $ describe \"Sample\" $ it \"should fit sample\" $"
+                  , "           shouldReturn (fmap (T.strip . T.pack) (readProcess (\"./\" ++ \"" ++ p ++ "\") [] U.sampleInput))"
+                  , "                        (T.pack U.sampleOutput)"
+                  , "main :: IO ()"
+                  , "main = U.main >> Main.sample" ]
+    runhaskell $ unlines mainSrc
+    
 usage :: FuncT
 usage _ = putStrLn "Usage: hrmng targets [c|r|t|x]"
 
--- src/HRank/Utilities/UnitTest.hs
+++
@@ -1,4 +0,0 @@
-module HRank.Utilities.UnitTest where
-
-runTest :: IO () -> IO () -> IO ()
-runTest s t = s
