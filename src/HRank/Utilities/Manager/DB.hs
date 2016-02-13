d1ef3c46083c68056bd04055d1c3c6e57b3f1bd4
Oopsno <yufeiliu94@gmail.com>
2016-02-14 00:54:54 +0800

    added & IOError handlers && fixed file lock

--- src/HRank/Utilities/Manager/DB.hs
+++ src/HRank/Utilities/Manager/DB.hs
@@ -1,10 +1,13 @@
 module HRank.Utilities.Manager.DB where
 
+import Control.Exception
 import Control.Monad
 import Data.Either
 import Data.Maybe
 import System.Directory
 import System.FilePath.Posix
+import System.IO
+import qualified System.IO.Strict as SIO
 
 absDBPath :: IO FilePath
 absDBPath = (</> ".hrmng/db.hs") <$> getHomeDirectory 
@@ -18,11 +21,18 @@ withFileExsit = withExsit doesFileExist
 withDirExsit :: (FilePath -> IO a) -> (FilePath -> IO a) -> FilePath -> IO a
 withDirExsit = withExsit doesDirectoryExist
 
+catchIOError :: String -> IO a -> IO a -> IO a
+catchIOError prefix action hock = catch action (\e ->
+  hPutStrLn stderr (prefix ++ ": IOError occurred: " ++ show (e :: IOError)) >> hock)
+
+catchIOError_ :: String -> IO () -> IO ()
+catchIOError_ p a = catchIOError p a (return ())
+
 readDB :: IO [(String, FilePath)]
-readDB = absDBPath >>= withFileExsit (\p -> read <$> readFile p) (\_ -> return [])
+readDB = catchIOError "[Manager][DB][readDB]" (absDBPath >>= withFileExsit (\p -> read <$> SIO.readFile p) (\_ -> return [])) (return [])
 
 writeDB :: [(String, FilePath)] -> IO ()
-writeDB xs = absDBPath >>= withFileExsit write (mkdir >> write)
+writeDB xs = catchIOError_ "[Manager][DB][writeDB]" (absDBPath >>= withFileExsit write (mkdir >> write))
   where mkdir = createDirectory . takeDirectory
         write = flip writeFile (show xs)
 
@@ -34,7 +44,7 @@ lookupDB :: String -> IO (Either String FilePath)
 lookupDB slug = eitherJust slug . lookup slug <$> readDB
 
 updateDB :: (String, FilePath) -> IO ()
-updateDB pair = (pair:) <$> readDB >>= writeDB
+updateDB pair = (pair:) <$> readDB >>= writeDB >> putStrLn ("[Manager][DB][updateDB]: added " ++ fst pair)
 
 nameOrPath :: String -> IO FilePath
 nameOrPath = withDirExsit return (lookupDB >=> (\m ->
