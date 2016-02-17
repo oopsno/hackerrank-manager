d869c1b79fceab524dcacf515b9335cb7ec85586
Oopsno <yufeiliu94@gmail.com>
2016-02-17 17:23:25 +0800

    removed 2 solutions...

--- src/HRank/Mathematics/Fundamentals/MinimumDraws/Solution.hs
+++
@@ -1,6 +0,0 @@
-module HRank.Mathematics.Fundamentals.MinimumDraws.Solution where
-
-import Control.Applicative
-import Control.Monad
-
-main = mapM_ print =<< (map (succ . read) . tail . lines <$> getContents :: IO [Int])
